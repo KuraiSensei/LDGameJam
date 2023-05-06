60e75234b47a128d97dc1a4c6a2d2570bf016202
Hamie <KuraiSensei@users.noreply.github.com>
2023-05-06 17:52:42 +0100

    Object Pick up+Rotate+Highlight added

---
+++ Assets/Scripts/ObjectMove/MoveObj.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class MoveObj : MonoBehaviour
+{
+   /* private bool isDragging = false;
+    private Vector3 startPosition;
+    private float distance;
+
+    public GameObject item;
+    public GameObject tempParent;
+
+    // Use this for initialization
+    void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            item.transform.position = rayPoint;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        distance = Vector3.Distance(item.transform.position, Camera.main.transform.position);
+        startPosition = item.transform.position;
+
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+
+        item.transform.parent = tempParent.transform;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+
+        item.transform.parent = null;
+        item.transform.position = startPosition;
+    }
+   */
+   
+    private bool isDragging = false;
+    private Vector3 startPosition;
+    private float distance;
+
+    public GameObject item;
+    public GameObject tempParent;
+
+    // Use this for initialization
+    void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            item.transform.position = rayPoint;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        distance = Vector3.Distance(item.transform.position, Camera.main.transform.position);
+        startPosition = item.transform.position;
+
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+
+        item.transform.parent = tempParent.transform;
+    }
+
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+
+        item.transform.parent = null;
+        item.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance));
+    }
+}
+
---
+++ Assets/Scripts/ObjectRotation/ObjRotation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjRotation : MonoBehaviour
+{
+    public float rotateSpeed = 50f;
+
+    void Update()
+    {
+        // rotate forward on W key press
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+        }
+
+        // rotate backward on S key press
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(-Vector3.right * rotateSpeed * Time.deltaTime);
+        }
+
+        // rotate to the right on D key press
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+
+        // rotate to the left on A key press
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
