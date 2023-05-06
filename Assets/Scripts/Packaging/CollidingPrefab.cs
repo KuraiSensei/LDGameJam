558849b96db2fb35ee098e582af5eb79780d7ec9
Hamie <KuraiSensei@users.noreply.github.com>
2023-05-07 00:10:02 +0100

    Tried collection point but didnt work
    Going to try again tomorrow, Also need to make sure WASD works only one one item at a time

--- Assets/Scripts/Packaging/CollidingPrefab.cs
+++ Assets/Scripts/Packaging/CollidingPrefab.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using UnityEngine;
 
 public class CollidingPrefab : MonoBehaviour
@@ -32,4 +32,83 @@ public class CollidingPrefab : MonoBehaviour
         }
     }
 }
+*/
+/*using System.Collections.Generic;
+using UnityEngine;
 
+public class CollidingPrefab : MonoBehaviour
+{
+    public List<GameObject> collidingObjects = new List<GameObject>();
+    public GameObject newPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Box(Clone)" && !collidingObjects.Contains(collision.gameObject))
+        {
+            collidingObjects.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Box(Clone)" && collidingObjects.Contains(collision.gameObject))
+        {
+            collidingObjects.Remove(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (collidingObjects.Count > 0)
+        {
+            gameObject.SetActive(false);
+            Instantiate(newPrefab, transform.position, transform.rotation);
+        }
+    }
+}
+*/
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidingPrefab : MonoBehaviour
+{
+    public List<GameObject> collidingObjects = new List<GameObject>();
+    public GameObject newPrefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Box(Clone)" && !collidingObjects.Contains(collision.gameObject))
+        {
+            collidingObjects.Add(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Box(Clone)" && collidingObjects.Contains(collision.gameObject))
+        {
+            collidingObjects.Remove(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (collidingObjects.Count > 0)
+        {
+            // Deactivate all objects in collidingObjects list
+            foreach (GameObject obj in collidingObjects)
+            {
+                if (obj.activeSelf)
+                {
+                    obj.SetActive(false);
+                }
+            }
+
+            // Instantiate newPrefab at the location of the first object in collidingObjects list
+            Instantiate(newPrefab, collidingObjects[0].transform.position, collidingObjects[0].transform.rotation);
+
+            // Clear collidingObjects list
+            collidingObjects.Clear();
+        }
+    }
+}
