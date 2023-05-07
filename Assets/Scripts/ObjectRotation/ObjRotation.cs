51426ce2e4e7c0df23e449cf6e0660367e3fd436
Hamie <KuraiSensei@users.noreply.github.com>
2023-05-07 14:39:38 +0100

    small edits

--- Assets/Scripts/ObjectRotation/ObjRotation.cs
+++ Assets/Scripts/ObjectRotation/ObjRotation.cs
@@ -11,30 +11,30 @@ public class ObjRotation : MonoBehaviour
     {
         if (selectedObject == gameObject)
         {
-            // rotate forward on W key press
-            if (Input.GetKey(KeyCode.W))
+            // rotate forward when pressing up arrow key or vertical axis is positive
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Vertical") > 0)
             {
                 transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
             }
 
-            // rotate backward on S key press
-            if (Input.GetKey(KeyCode.S))
+            // rotate backward when pressing down arrow key or vertical axis is negative
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("Vertical") < 0)
             {
                 transform.Rotate(-Vector3.right * rotateSpeed * Time.deltaTime);
             }
 
-            // rotate to the right on D key press
-            if (Input.GetKey(KeyCode.D))
+            // rotate to the right when pressing right arrow key or horizontal axis is positive
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Horizontal") > 0)
             {
                 transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
             }
 
-            // rotate to the left on A key press
-            if (Input.GetKey(KeyCode.A))
+            // rotate to the left when pressing left arrow key or horizontal axis is negative
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Horizontal") < 0)
             {
                 transform.Rotate(-Vector3.up * rotateSpeed * Time.deltaTime);
             }
         }
     }
-
 }
+
